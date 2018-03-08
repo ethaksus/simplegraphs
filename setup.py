b50ad7ad62424300eb30e65839e2a12f89094ffc
damouryouknow <damourba@gmail.com>
2018-03-07 19:32:46 -0500

    package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='simplegraphs',
+      version='0.1',
+      description='Simple tools for building and analyzing graphs',
+      url='https://github.com/DamourYouKnow/simplegraphs',
+      author='DamourYouKnow',
+      license='MIT',
+      packages=['simplegraphs'],
+      zip_safe=False)
